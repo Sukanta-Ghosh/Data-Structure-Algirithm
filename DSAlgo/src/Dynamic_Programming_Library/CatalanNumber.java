0b059f297ba28fc331ff8de8b32e408bf048b92e
Sukanta Ghosh <48876856+Sukanta-Ghosh@users.noreply.github.com>
2023-07-26 01:24:53 +0530

    Update CatalanNumber code comment

--- DSAlgo/src/Dynamic_Programming_Library/CatalanNumber.java
+++ DSAlgo/src/Dynamic_Programming_Library/CatalanNumber.java
@@ -3,12 +3,12 @@ package Dynamic_Programming_Library;
 /* 
 Intersecting Chords in a Circle: https://www.scaler.com/academy/mentee-dashboard/class/70877/homework/problems/339?navref=cl_tt_lst_sl
 Unique Binary Search Trees: https://www.scaler.com/academy/mentee-dashboard/class/70877/assignment/problems/22?navref=cl_tt_nv */
-public class IntersectingChordsCircle {
+public class CatalanNumber {
     /*
      * T.C: O(n ^ 2)
      * S.C: O(n)
      * Catalan Number: Recurrence relation =>
-     * T(n) = T(0) * T(n - 1) + T(1) * T(n - 2) ....T(n - 1) * T(0)
+     * T(n) = T(0) * T(n - 1) + T(1) * T(n - 2) + .... + T(n - 1) * T(0)
      */
     public int catalanNumber(int A) {
         int mod = 1000000007;
@@ -25,7 +25,10 @@ public class IntersectingChordsCircle {
         /* Iterate to find recurrence relation */
         for (int i = 2; i <= A; i++) {
             long val = 0;
-            /* Iterate j from 0-i */
+            /*
+             * Iterate j from 0-i
+             * i - j - 1 => Put in catalan no equation
+             */
             for (int j = 0; j < i; j++) {
                 val = (val + (long) dp[j] * dp[i - j - 1]) % mod;
             }
